94ce8334b6215a11c6a664080b2f569666322c02
Fer VT <fer.vargas.torres@gmail.com>
2021-12-21 20:40:26 -0600

    Add explosion particles effect on goal

--- Entities/Goal/Goal.cs
+++ Entities/Goal/Goal.cs
@@ -7,9 +7,16 @@ public class Goal : Area2D {
 	[Export]
 	private bool _isRight;
 
-	public override void _Ready() {}
+	private Particles2D _particles2D;
+
+	public override void _Ready() {
+		_particles2D = GetNode<Particles2D>("Particles2D");
+	}
 
 	private void OnGoal_BodyEntered(KinematicBody2D body) {
+		_particles2D.GlobalPosition = body.Position;
+		_particles2D.Restart();
+
 		EmitSignal(nameof(GoalScored), _isRight);
 	}
 }
